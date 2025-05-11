d7d5836fe6c89636d0fd56f012af07642148528f
SUKALPA BHATTACHARYYA <71346070+SUKALPA108@users.noreply.github.com>
2025-05-11 16:36:33 +0530

    Delete communicationservice/communicationservice directory

--- communicationservice/communicationservice/src/main/java/com/hrms/communicationservice/service/MessageService.java
+++
@@ -1,9 +0,0 @@
-package com.hrms.communicationservice.service;
-
-import com.hrms.communicationservice.entity.Message;
-import java.util.List;
-
-public interface MessageService {
-    Message sendMessage(Message message);
-    List<Message> getAllMessages();
-}
